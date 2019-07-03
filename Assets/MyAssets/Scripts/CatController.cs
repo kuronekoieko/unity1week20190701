10322c3026926afe2c546b5ee17e164fe75788b1
ieko <mendo931411.mou@gmail.com>
2019-07-04 00:24:59 +0900

    ジャンプ処理そ実装(さっきのは左右移動)

--- Assets/MyAssets/Scripts/CatController.cs
+++ Assets/MyAssets/Scripts/CatController.cs
@@ -13,29 +13,73 @@ public class CatController : MonoBehaviour
     {
         rb = GetComponent<Rigidbody>();
         i = this;
+        keyX = 0;
     }
 
     // Update is called once per frame
     public void FixedUpd()
     {
         float x = keyX * 6;
-        rb.velocity = new Vector3(x, 0, Values.CAT_SPEED);
+        rb.velocity = new Vector3(x, rb.velocity.y, Values.CAT_SPEED);
         //Debug.Log(rb.velocity);
     }
 
     public void Upd()
     {
+        HorizontalController();
+        JumpController();
+
+    }
+
+    void JumpController()
+    {
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (!IsGround()) { return; }
+            rb.velocity += new Vector3(0, 7, 0);
+        }
+    }
+
+    void HorizontalController()
+    {
         keyX = 0;
         if (Input.GetKey(KeyCode.RightArrow))
         {
             keyX = 1;
-            Debug.Log(rb.velocity);
         }
         if (Input.GetKey(KeyCode.LeftArrow))
         {
             keyX = -1;
         }
+    }
 
+    bool IsGround()
+    {
+        //Rayの作成　　　　　　　↓Rayを飛ばす原点　　　↓Rayを飛ばす方向
+        Ray ray = new Ray(transform.position, new Vector3(0, -1, 0));
+
+        //Rayが当たったオブジェクトの情報を入れる箱
+        RaycastHit hit;
+
+        //Rayの飛ばせる距離
+        float distance = 0.1f;
+
+        //Rayの可視化    ↓Rayの原点　　　　↓Rayの方向　　　　　　　　　↓Rayの色
+        Debug.DrawLine(ray.origin, ray.origin + ray.direction * distance, Color.red);
+
+        //もしRayにオブジェクトが衝突したら
+        //                  ↓Ray  ↓Rayが当たったオブジェクト ↓距離
+        if (Physics.Raycast(ray, out hit, distance))
+        {
+            //Rayが当たったオブジェクトのtagがPlayerだったら
+            if (hit.collider)
+            {
+                Debug.Log("接地");
+            }
+            return hit.collider;
+        }
+        return false;
     }
 
 }
