9214893a9ff4f95e32179bce8244b30714c4a4fc
ieko <mendo931411.mou@gmail.com>
2019-07-07 01:22:56 +0900

    アニメーションのスピードを調整

--- Assets/MyAssets/Scripts/CatController.cs
+++ Assets/MyAssets/Scripts/CatController.cs
@@ -48,7 +48,7 @@ public class CatController : MonoBehaviour
         HorizontalController();
         JumpController();
         animator.SetTrigger("Run");
-        animator.speed = !IsGround() ? 0.1f : 1;
+        animator.speed = !IsGround() ? 0.1f : 1.5f;
 
     }
 
