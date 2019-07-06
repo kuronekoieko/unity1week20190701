b0a0d1859fcfcc2a4fa31ffdc0a2f4b3d0cd9970
ieko <mendo931411.mou@gmail.com>
2019-07-07 00:41:17 +0900

    カウントダウンを作成

--- Assets/MyAssets/Scripts/GameManager.cs
+++ Assets/MyAssets/Scripts/GameManager.cs
@@ -10,25 +10,83 @@ public class GameManager : MonoBehaviour
     [SerializeField] CameraController cam;
     [SerializeField] Text hearNumText;
     [SerializeField] EnemyManager enemyManager;
+    [SerializeField] Text countDownText;
+    [SerializeField] Text spaceDownText;
+    float countDownNum;
+    GameState gameState;
 
     void Start()
     {
         cat.Init();
         cam.Init();
         enemyManager.Init();
+        countDownNum = 5f;
     }
 
     void FixedUpdate()
     {
-        cat.FixedUpd();
+
+        switch (gameState)
+        {
+            case GameState.WAITING:
+
+                break;
+            case GameState.COUNT_DOWN:
+
+                break;
+            case GameState.PLAY:
+                cat.FixedUpd();
+                break;
+            case GameState.RESULT:
+
+                break;
+
+            default:
+                break;
+        }
     }
 
     void Update()
     {
-        cam.Upd();
-        cat.Upd();
-        enemyManager.Upd();
+
         hearNumText.text = Variables.getHeartNum.ToString();
+        switch (gameState)
+        {
+            case GameState.WAITING:
+                if (Input.GetKeyDown(KeyCode.Space))
+                {
+                    spaceDownText.gameObject.SetActive(false);
+                    gameState = GameState.COUNT_DOWN;
+                }
+                break;
+            case GameState.COUNT_DOWN:
+                CountDown();
+                break;
+            case GameState.PLAY:
+                cam.Upd();
+                cat.Upd();
+                enemyManager.Upd();
+                break;
+            case GameState.RESULT:
+
+                break;
+
+            default:
+                break;
+        }
+
+    }
+
+    void CountDown()
+    {
+        countDownNum -= Time.deltaTime;
+        float num = Mathf.Ceil(countDownNum);
+        countDownText.text = num.ToString("F0");
+        if (num == 0)
+        {
+            countDownText.gameObject.SetActive(false);
+            gameState = GameState.PLAY;
+        }
     }
 
 
